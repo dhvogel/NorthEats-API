ed1d362f3bb417523015e45bcf6a7d94c5e7f831
dhvogel <dhvogel2468@gmail.com>
2016-11-05 17:48:40 -0400

    added server.js

---
+++ src/server.js
@@ -0,0 +1,32 @@
+//server.js
+
+//modules
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var aws = require('aws-sdk');
+
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+//parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//set the static files location. so /public/index.html will be just index.html for users
+app.use(express.static(__dirname + "/public"));
+
+//routes
+var routes = require('./routes/index');
+
+//start app at whatever port we specified
+var server = app.listen(port);
+
+//let the user know whas goin on
+console.log("Magic happens on port " + port);
+
+exports = module.exports = app;
