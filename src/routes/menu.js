62de47c7243d23f7fc5dcd8e685ca6b5ef86caf7
dhvogel <dhvogel2468@gmail.com>
2016-11-09 22:54:36 -0500

    added min and max for required option selections

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -13,7 +13,6 @@ var dynamodbClient = new aws.DynamoDB.DocumentClient();
 exports.postMenu = function(req, res) {
   var restaurantId = req.params.restaurantId;
 
-  console.log(req.body.menuItems);
   var params = {
     TableName: TABLE_NAME,
     Item: {
@@ -26,14 +25,14 @@ exports.postMenu = function(req, res) {
   dynamodbClient.put(params, function(err, data) {
     if (err) {
       return res.status(400).json({
-        success:false,
+        success: false,
         error: err
       })
     }
     else {
       return res.status(200).json({
-        success:true,
-        data:data
+        success: true,
+        data: params
       })
     };
   });
