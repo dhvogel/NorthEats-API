5e3226e1cb93b4e647316353a62279edee5ee601
Robert Durst <robert@deliveryrocket.com.au>
2016-12-04 14:57:50 -0500

    Case conventions and check for already existant restaurant

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -47,37 +47,171 @@ POST /restaurant
 */
 exports.postRestaurant = function(req, res) {
 	
-    var params = {
-      TableName: TABLE_NAME,
-	  Item: {
-	        //restaurantId : (Math.random()*100000000000000000).toString(),
-		  	restaurantId: req.body.displayName+req.body.city+req.body.state,
-			description: req.body.description,
-		  	displayName: req.body.displayName,
-		  	email: req.body.email,
-		  	phone: req.body.phone,
-		    city: req.body.city,
-		    state: req.body.state
-		  	
-	      }
+	
+	
+	//Check the size of the display name
+	if (req.body.displayName.length == 0){
+  	  
+		  var err = "Display name not found."
 	  
-    };
+		  return res.status(417).json({
+	  	          success: false,
+	  	          error: err
+	  	        })
+		}
+	
+	//Check the size of the city
+	if (req.body.city.length == 0){
+  	  
+		  var err = "City not found."
+	  
+		  return res.status(417).json({
+	  	          success: false,
+	  	          error: err
+	  	        })
+		}
+	
+	//Check the size of the state
+	if (req.body.state.length == 0){
+  	  
+		  var err = "State not found."
+	  
+		  return res.status(417).json({
+	  	          success: false,
+	  	          error: err
+	  	        })
+		}
+	
+		//Check that the phone number is in the correct format/reformat it and right length
+		var phone = (req.body.phone.replace(/[\s-()]/g,""));
+	
+		if(phone.length == 0){
+	  		var err = "Phone number not found."
+	  
+	  		return res.status(417).json({
+	    	          success: false,
+	    	          error: err
+	    	        })
+		}
+	
+		else if(phone.length != 10){
+	  		var err = "Phone number incorrect length."
+	  
+	  		return res.status(417).json({
+	    	          success: false,
+	    	          error: err
+	    	        })
+		}
+	
+		//Check that the email contains the @ character
+		if(req.body.email.length == 0){
+	  		var err = "Email not found."
+	  
+	  		return res.status(415).json({
+	    	          success: false,
+	    	          error: err
+	    	        })
+		}
+	
+		else if(req.body.email.includes('@') == false){
+	  		var err = "Email format incorrect."
+	  
+			res.status(417).json({
+				    	          success: false,
+				    	          error: err
+				    	        })
+		}
+	
+	//Check the size of the description
+	if (req.body.description.length > 200){
+      
+	  var err = "Description too long. Please Limit to 200 characters."
+	  
+	  return res.status(431).json({
+	          success: false,
+	          error: err
+	        })
+	}
+	
+	else if (req.body.description.length == 0){
+  	  
+	  var err = "Description not found."
+	  
+	  return res.status(417).json({
+  	          success: false,
+  	          error: err
+  	        })
+	}
+	
+	//Correct the characters of the display name, city, state
+	var displayNameForID = (req.body.displayName.replace(/\s+/g, '_').toLowerCase());
+	var stateForID = (req.body.state.replace(/\s+/g, '').toLowerCase());
+	var cityForID = (req.body.city.replace(/\s+/g, '').toLowerCase());
+	
+	//The Date time
+	var date = new Date().getTime()
+	
+	
+	var params = {
+	    TableName: TABLE_NAME,
+	    KeyConditionExpression: "restaurantId = :restaurantId",
+	    ExpressionAttributeValues: {
+	        ":restaurantId": displayNameForID+"-"+cityForID+"-"+stateForID
+	    }
+	};
+
+	dynamodbClient.query(params, function(err, data) {
+	    if (err)
+	        console.log(JSON.stringify(err, null, 2));
+	    else{
+			
+			if(data["Count"] == 1){
+		      
+			  var error = "Restaurant already exists for that name, city, and state."
+			  
+			  return res.status(409).json({
+		        success: false,
+		        data: error
+		      })
+			}
+			
+			else{
+			    var params = {
+			      TableName: TABLE_NAME,
+				  Item: {
+					  	restaurantId: displayNameForID+"-"+cityForID+"-"+stateForID,
+						description: req.body.description,
+					  	displayName: req.body.displayName,
+					  	email: req.body.email,
+					  	phone: phone,
+					    city: req.body.city,
+					    state: req.body.state,
+					    dateTime: date
+				      }
+	  
+			    };
 	
  
-	dynamodbClient.put(params, function(err, data) {
-	    if (err) {
-	      return res.status(500).json({
-	        success: false,
-	        error: err
-	      })
-	    }
-	    else {
-	      return res.status(200).json({
-	        success: true,
-	        data: data
-	      })
-	    };
-	  });
+				dynamodbClient.put(params, function(err, data) {
+				    if (err) {
+				      return res.status(500).json({
+				        success: false,
+				        error: err
+				      })
+				    }
+				    else {
+				      return res.status(200).json({
+				        success: true,
+				        data: params
+				      })
+				    };
+				  });
+			}
+		}
+		});
+	
+	
+    
   
 }
 
@@ -87,8 +221,6 @@ GET /restaurant/:restaurantId
 exports.getRestaurantById = function(req, res) {
   var restaurantId = req.params.restaurantId;
 
-  console.log(restaurantId)
-
   var params = {
     TableName: TABLE_NAME,
     Key: {
@@ -97,7 +229,6 @@ exports.getRestaurantById = function(req, res) {
   };
 
   dynamodbClient.get(params, function(err, data) {
-      console.log(data)
       if (err) {
           return res.status(400).json({
             success: false,
@@ -110,14 +241,12 @@ exports.getRestaurantById = function(req, res) {
         });
       }
       else {
-          return res.status(200).json({
+          return res.status(403).json({
             success: true,
             data: data
           });
       }
   });
-
-  console.log(params)
 }
 
 /*
